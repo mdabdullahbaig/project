060ba0daf6e4b0fb11033964cb13a0a3ad22dd2b
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2019-04-03 23:54:39 +0530

    Add sidebar navigation bar with dropdown

--- public/js/drawer.js
+++ public/js/drawer.js
@@ -1,3 +1,7 @@
-function toggleSidebar() {
-    document.getElementById("sidebar").classList.toggle("active");
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
