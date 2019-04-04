f3335fe3b352a41d4e937e50dbb7cb01843d54df
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2019-04-04 20:03:21 +0530

    Add product model

--- routes/product-routes.js
+++
@@ -1,5 +0,0 @@
-// const router = require('express').Router();
-
-// router.get('/', isloggin,(req,res) => {
-//     res.render('addproduct',{ user: req.user } );
-// });
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const Product = require('../models/product');
 
 const authCheck = (req, res, next) => {
-    if(!req.user){
+    if (!req.user) {
         res.redirect('/auth/login');
     } else {
         next();
@@ -9,11 +10,41 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.render('index', { user: req.user });
+    res.render('index', {
+        user: req.user
+    });
 });
 
 router.get('/addproduct', authCheck, (req, res) => {
-    res.render('addproduct', { user: req.user });
+    res.render('addproduct', {
+        user: req.user
+    });
 });
+
+router.post('/addproduct', authCheck, (req, res) => {
+   
+       var productname = req.body.productname;
+        var imgurl01 = req.body.imgurl01;
+        var imgurl02 = req.body.imgurl02;
+        var imgurl03 = req.body.imgurl03;
+        var imgurl04 = req.body.imgurl04;
+        var price = req.body.price;
+
+        var newProduct = {productname: productname,
+                            imgurl01:imgurl01,
+                             imgurl02:imgurl02,
+                             imgurl03:imgurl03,
+                             imgurl04:imgurl04,
+                             price:price };
+
+        Product.create(newProduct, function(err, newlyCreated) {
+            if(err) {
+                console.log(err);
+            } else {
+                res.redirect('/home');   
+            }
+        })
+    });
+
 
 module.exports = router;
